c4ceb1f70336cf630390c74269996bc54dd1c1db
Elena <elender1230@gmail.com>
2024-04-07 18:27:12 +0200

    add infoBox compoenent, update css style

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-course
 React course from Udemy
+
+Start the project with:
+npm install
+npm run dev
--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal.tsx";
 import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
+import { type ReactNode } from "react";
 
 type CoursefoalListProps = {
   goals: CGoal[]
@@ -8,7 +10,17 @@ type CoursefoalListProps = {
 
 
 export default function CourseGoalList({ goals, onDeleteGoal }: CoursefoalListProps) {
+  if(goals.length === 0) {
+    return <InfoBox mode="hint"> You haver no goals :/</InfoBox>
+  }
+  let warningBox: ReactNode
+  if(goals.length >= 4){
+    warningBox = <InfoBox mode="warning" severity="medium">No te pases wey</InfoBox>
+  }
+
   return (
+    <>
+    {warningBox}
     <ul>
       {goals.map((goal) => <li>
         <CourseGoal
@@ -19,5 +31,6 @@ export default function CourseGoalList({ goals, onDeleteGoal }: CoursefoalListPr
         </CourseGoal>
       </li>)}
     </ul>
+    </>
   );
 }
